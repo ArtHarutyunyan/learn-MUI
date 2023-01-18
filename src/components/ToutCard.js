a644fdb1b10b554f82559a7dfab71b5ab2b53b69
ArtHarutyunyan <55248428+ArtHarutyunyan@users.noreply.github.com>
2023-01-18 19:14:10 +0400

    add enithing

--- src/components/ToutCard.js
+++ src/components/ToutCard.js
@@ -20,7 +20,9 @@ const theme = createTheme({
         {
           props: { variant: "body3" },
 
-          style: { frontSize: 9 },
+          style: {
+            fontSize: 9,
+          },
         },
       ],
     },
@@ -30,54 +32,57 @@ const theme = createTheme({
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          className="img"
-          src="https://cs.copart.com/v1/AUTH_svc.pdoc00001/LPP501/0757c8e8b0dc4bd8b267bebc1f817537_ful.jpg"
-        ></img>
-      </Paper>
-      <Box paddingX={1}>
-        <Typography variant="subtitle1" component="h2">
-          2023 LEXUS IS 300
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <AccessTime sx={{ width: 12.5 }} />
-          <Typography variant="body2" component="p" marginLeft={0.5}>
-            5 hours
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-          }}
-          marginTop={3}
-        >
-          <Rating
-            name="read-only"
-            value={4.5}
-            readOnly
-            precision={0.5}
-            size="small"
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            className="img"
+            alt=""
+            src="https://cs.copart.com/v1/AUTH_svc.pdoc00001/LPP501/0757c8e8b0dc4bd8b267bebc1f817537_ful.jpg"
           />
-          <Typography variant="body2" component="p" marginLeft={0.5}>
-            4.5
-          </Typography>
-          <Typography variant="body3" component="p" marginLeft={1.5}>
-            (655 reviews)
-          </Typography>
-        </Box>
-        <Box>
-          <Typography variant="h6" component="h3" marginTop={0}>
-            Odometer: 9,095 mi (ACTUAL)
-          </Typography>
-        </Box>
-      </Box>
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              2023 LEXUS IS 300
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                5 hours
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+              marginTop={3}
+            >
+              <Rating
+                name="read-only"
+                value={4.5}
+                readOnly
+                precision={0.5}
+                size="small"
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                4.5
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={1.5}>
+                (655 reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
+                Odometer: 9,095 mi (ACTUAL)
+              </Typography>
+            </Box>
+          </Box>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
